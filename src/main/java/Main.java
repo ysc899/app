6fee8a69a4297d72f924a526be26357ed03a9155
ysc899 <ysc899@gmail.com>
2021-03-06 20:58:30 +0900

    hello

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello ysc");
+        Person person = new Person("ysc899");
+        System.out.println(person.getName());
+    }
+}
